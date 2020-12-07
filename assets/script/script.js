77314132dbd94fe45c81f4375077d8bcfaaef346
yusufsofyan <yusuf_sofyan@outlook.com>
2020-12-07 14:41:17 +0700

    all done

--- assets/script/script.js
+++ assets/script/script.js
@@ -39,22 +39,45 @@ $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
 
   if (wScroll > $('.about-us').offset().top-500) {
-    $('.about-plus img').each(function(i){
-      setTimeout(function(){
-        $('.about-plus img').eq(i).addClass('show');
-      }, 300 * (i+1))
-    });
+    $('.about-us .tittle').addClass('show');
+    
+    setTimeout(function(){
+      $('.about-us p').addClass('show');
+    },200)
+    
+    setTimeout(function(){
+      $('.about-plus img').each(function(i){
+        setTimeout(function(){
+          $('.about-plus img').eq(i).addClass('show');
+        }, 300 * (i+1))
+      },5000);
+    })
   }
-
+  
   if (wScroll > $('.product').offset().top-500) {
+
+    setTimeout(function(){
+      $('.product .tittle').addClass('show');
+    },150)
+    
+    
     $('.galery img').each(function(i){
       setTimeout(function(){
         $('.galery img').eq(i).addClass('show');
       }, 200 * (i+1))
     });
+
+    setTimeout(function(){
+      $('.product a').addClass('show');
+    },2000)
   }
 
   if (wScroll > $('.testimoni').offset().top-500) {
+
+    setTimeout(function(){
+      $('.testimoni .tittle').addClass('show');
+    },150)
+
     $('.testi-container .testi-card').each(function(i){
       setTimeout(function(){
         $('.testi-container .testi-card').eq(i).addClass('show');
@@ -62,11 +85,55 @@ $(window).scroll(function () {
     });
   }
 
+  if (wScroll > $('.client').offset().top-500) {
+
+    setTimeout(function(){
+      $('.client .tittle').addClass('show');
+    },150)
+
+    $('.testi-container .testi-card').each(function(i){
+      setTimeout(function(){
+        $('.testi-container .testi-card').eq(i).addClass('show');
+      }, 200 * (i+1))
+    });
+
+    setTimeout(function(){
+      $('.client img').addClass('show');
+    },600)
+  }
+
   if (wScroll > $('.howto').offset().top-500) {
+
+    setTimeout(function(){
+      $('.howto .tittle').addClass('show');
+    },150)
+
     $('.order-container .order-card').each(function(i){
       setTimeout(function(){
         $('.order-container .order-card').eq(i).addClass('show');
       }, 200 * (i+1))
     });
+
+    setTimeout(function(){
+      $('.howto h5').addClass('show');
+    },1600)
+
+    setTimeout(function(){
+      $('.howto a').addClass('show');
+    },1600)
   }
+
+  if (wScroll > $('.footer').offset().top-800) {
+
+    setTimeout(function(){
+      $('.footer .action-one').addClass('show');
+    },300)
+
+    setTimeout(function(){
+      $('.footer .action-two').addClass('show');
+    },300)
+
+  }
+
+
 });
